065071f63069e0f356128d8d095c62c9828a98c6
nhuntera <109431292+nhuntera@users.noreply.github.com>
2024-08-30 11:42:12 -0500

    Update demo.py

--- demo.py
+++ demo.py
@@ -2,10 +2,13 @@ import DotGL
 import time
 import random
 
+width = 180
+hight = 50
+
 #add "True" to use pixel width compensation
 #if used use half the horizontal resolution you would
-draw = DotGL.Screen(180,52)
-#draw = DotGl.Screen(90, 52, True)
+draw = DotGL.Screen(width, hight)
+#draw = DotGL.Screen(width, hight, True)
 
 #draw.debug()
 
@@ -16,26 +19,26 @@ ball_x_momentum=1
 ball_y_momentum=0
 while(True):
 
-    time.sleep(.01)
-    draw.line(0,0,179,0)
-    draw.line(179,0,179,51)
-    draw.line(179,51,0,51)
-    draw.line(0,51,0,0)
+    time.sleep(.03)
+    draw.line(0,0,width-1,0)
+    draw.line(width-1,0,width-1,hight-1)
+    draw.line(width-1,hight-1,0,hight-1)
+    draw.line(0,hight-1,0,0)
     
     if ball_x < 1:
         ball_x_momentum=1
         ball_y_momentum=random.triangular(-1,1,0)
-    if ball_x > 178:
+    if ball_x > width-2:
         ball_x_momentum=-1
         ball_y_momentum=random.triangular(-1,1,0)
-    if ball_y < 1 or ball_y > 51: ball_y_momentum = ball_y_momentum *-1
+    if ball_y < 1 or ball_y > hight-1: ball_y_momentum = ball_y_momentum *-1
     ball_x= ball_x + ball_x_momentum
     ball_y= ball_y + ball_y_momentum
 
     #both pixel and line can take an argument to change the character being used
     #this line() will make a line from the balls locaton to 0,0 and will use a "+" instead of the default "█"
     #draw.line(ball_x,int(ball_y),0,0, "+")
-    draw.pixel(ball_x,int(ball_y))
+    draw.pixel(int(ball_x),int(ball_y))
     
     #draws the screen and manualy clears
     draw.update()
